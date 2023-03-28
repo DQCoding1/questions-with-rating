eed7aecfd687d5b584a43ed82f0f4db98c3c2674
DQCoding1 <diegoabquintero@gmail.com>
2023-03-27 23:20:20 -0500

    javascript change question by question

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//go from header to main 
 const header = document.querySelector(".header")
 const main = document.querySelector(".main")
 const startSurveyBtn = document.querySelector(".header__button")
@@ -8,3 +9,32 @@ const startSurvey = () => {
 }
 
 startSurveyBtn.addEventListener("click", startSurvey)
+
+
+
+
+
+//change question by question
+const ratingBtns = document.querySelectorAll(".main__buttonRating")
+const question = document.querySelectorAll(".main__question")
+const footer = document.querySelector(".footer")
+
+const goToNextQuestion = () => {
+  for (let i = 0; i < question.length; i++){
+    if (question[i].classList.contains("visible")) {
+      if (i === question.length-1){
+        main.classList.remove("visible")
+        footer.classList.add("visible")
+      } else {
+        question[i].classList.remove("visible")
+        question[i+1].classList.add("visible")
+        break;
+      }
+    }
+  }
+}
+
+ratingBtns.forEach((btn) => {
+  btn.addEventListener("click", goToNextQuestion)
+})
+
